ccef122d10e6ed2bb5a531f3ee5dad952096095a
Zsolti <Zsolti@DESKTOP-SHB3U8E>
2020-08-03 21:59:10 +0200

    Basic solution

---
+++ NumberConverter/NumberConverter/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NumberConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var romanToInt = new RomanToIntegerConverter();
+            var output = romanToInt.RomanToInteger(input.ToUpper());
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ NumberConverter/NumberConverter/RomantoIntegerConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumberConverter
+{
+    public class RomanToIntegerConverter
+    {
+        private static Dictionary<char, int> RomanIntegerDictionary = new Dictionary<char, int>()
+        {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+
+        public int RomanToInteger(string roman)
+        {
+            int number = 0;
+            char previousChar = roman[0];
+            foreach (char currentChar in roman)
+            {
+                number += RomanIntegerDictionary[currentChar];
+                if (RomanIntegerDictionary[previousChar] < RomanIntegerDictionary[currentChar])
+                {
+                    number -= RomanIntegerDictionary[previousChar] * 2;
+                }
+                previousChar = currentChar;
+            }
+            return number;
+        }
+    }
+}
